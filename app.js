c7a3f5f20755baa0d1971bc91813a47488c272ae
Alexig2 <159358630+Alexig2@users.noreply.github.com>
2024-12-04 15:10:01 -0400

    Desafio - #3 - endpoint pokemons

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.get('/tico', (req, res) => {
     res.send('teco')
 })
 
+app.get('/tico', (req, res) => {
+    const pokemons = ['1- Pikachu', '2- Caterpie', '3- Pidgeotto', '4- Bulbasaur', '5- Charmander', '6- Squirtle', '7- Krabby', '8- Raticate', '9- Primeape', '10- Muk']
+    res.json(pokemons)
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
